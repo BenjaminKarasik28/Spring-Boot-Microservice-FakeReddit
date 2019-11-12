577e09d96262310f6907ccca10669c9ee16cc6ff
Tracy Chen <stchen1012@gmail.com>
2019-11-11 23:48:25 -0500

    login and signup tested. user-api doesn't run with test

--- user-api/src/main/java/com/example/userapi/repository/UserRepository.java
+++ user-api/src/main/java/com/example/userapi/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.userapi.repository;
 import com.example.userapi.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
