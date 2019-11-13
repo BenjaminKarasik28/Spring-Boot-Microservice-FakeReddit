99ed415889e448b9e8bd0b43c1431c878df65b4d
Tracy Chen <stchen1012@gmail.com>
2019-11-13 12:38:21 -0500

    started to added security to api gateway

---
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.apigateway.config;
+
+import com.example.userapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers( "/user/login/**").permitAll()
+                .antMatchers("/user/signup/**").permitAll()
+                .antMatchers("/post/list/**").permitAll()
+                .antMatchers("/post/**").authenticated()
+                .antMatchers("/comment/**").authenticated()
+                .antMatchers("/user/**").authenticated()
+                // .antMatchers("/profile/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    UserService userService;
+
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Bean("encoder")
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
