f90857186869f88bb1558daee4fe6db4aaa6374a
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-11-18 13:54:08 -0500

    fixing api calls

--- user-api/src/main/java/com/example/userapi/service/UserServiceImpl.java
+++ user-api/src/main/java/com/example/userapi/service/UserServiceImpl.java
@@ -86,8 +86,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUserByUsername(String username) {
 
-        restTemplate.delete("http://localhost:8082/post/" + username);
-        restTemplate.delete("http://localhost:8083/post/name/" + username);
+        restTemplate.delete("http://post-api:8082/post/" + username);
+        restTemplate.delete("http://comment-api:8083/post/name/" + username);
         User savedUser = userRepository.findByUsername(username);
         userRepository.delete(savedUser);
     }
