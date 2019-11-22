1da36add2306ab35639709ee6dc4b7d4c94bb7ea
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-11-22 14:53:43 -0500

    receivernset up

---
+++ email-api/src/main/java/com/example/emailapi/config/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.example.emailapi.config;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class RabbitMQConfig {
+    private final static String QUEUE_NAME = "queue1";
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false, false, false);
+    }
+}
