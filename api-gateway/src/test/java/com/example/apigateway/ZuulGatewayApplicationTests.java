18b3344668f7a1231624ba5cce942a7463b756c2
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-11-27 11:11:24 -0500

    config

--- api-gateway/src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.apigateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 public class ZuulGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testTest(){
+		assertEquals(1, 1);
+
 	}
 
 }
--- email-api/src/test/java/com/ga/emailapi/EmailApiApplicationTests.java
+++ email-api/src/test/java/com/ga/emailapi/EmailApiApplicationTests.java
@@ -3,11 +3,18 @@ package com.ga.emailapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class EmailApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testTest(){
+		assertEquals(1, 1);
+
 	}
 
 }
