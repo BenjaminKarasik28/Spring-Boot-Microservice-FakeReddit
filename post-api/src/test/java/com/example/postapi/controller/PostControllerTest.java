4ed8bee014e04156d3dd1fb3087c9a7279a80b55
Tracy Chen <stchen1012@gmail.com>
2019-12-01 13:34:07 -0500

    added exception post test in controller

--- post-api/src/test/java/com/example/postapi/controller/PostControllerTest.java
+++ post-api/src/test/java/com/example/postapi/controller/PostControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.postapi.controller;
 
+import com.example.postapi.exceptionhandling.BlankPostException;
 import com.example.postapi.model.Post;
 import com.example.postapi.model.PostComment;
 import com.example.postapi.service.PostService;
@@ -13,6 +14,7 @@ import org.junit.Before;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -172,10 +174,24 @@ public class PostControllerTest {
                 .andReturn();
     }
 
-
-
-
     private static String createPostInJson(String title, String desc) {
         return "{ \"title\": \"" + title + "\", " + "\"description\":\"" + desc  +"\" }";
     }
+
+    @Test
+    public void blankPostException_400_Fail() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createPostInJson("", ""));
+
+//        when(postService.createPost(any(),any())).thenThrow(new BlankPostException("Empty"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+
 }
+
