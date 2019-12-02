68d2730fd32c530d8e674f5738b2d730335d5312
Tracy Chen <stchen1012@gmail.com>
2019-12-02 12:13:01 -0500

    added comments for javadoc user-api

--- user-api/src/main/java/com/example/userapi/service/ProfileService.java
+++ user-api/src/main/java/com/example/userapi/service/ProfileService.java
@@ -4,11 +4,29 @@ import com.example.userapi.model.Profile;
 
 public interface ProfileService {
 
-
+    /**
+     * Creates and saves user profile
+     *
+     * @param newProfile the user profile to be created
+     * @return the newly created user profile
+     */
     public Profile createProfile(String username, Profile newProfile);
 
+    /**
+     * Gets user profile
+     *
+     * @param username name of user
+     * @return the profile of user
+     */
     public Profile getProfile(String username);
 
+    /**
+     * Updates profile
+     *
+     * @param username name of user
+     * @param updateProfile updated profile information
+     * @return the update profile
+     */
     public Profile updateProfile(String username, Profile updateProfile);
 
 //    public Profile createProfile(String username, Profile newProfile);
--- user-api/src/main/java/com/example/userapi/service/UserRoleService.java
+++ user-api/src/main/java/com/example/userapi/service/UserRoleService.java
@@ -7,11 +7,34 @@ import java.util.List;
 
 public interface UserRoleService {
 
+    /**
+     * Creates and saves user role
+     *
+     * @param newRole the user role to be created
+     * @return the newly created user role
+     */
     public UserRole createRole(UserRole newRole);
 
+    /**
+     * Gets list of roles
+     *
+     * @return list of roles
+     */
     public List<UserRole> listRoles();
 
+    /**
+     * Retrieve a specific role
+     *
+     * @param roleName name of role
+     * @return specific role
+     */
     public UserRole getRole(String roleName);
 
+    /**
+     * Get username by email
+     *
+     * @param email email address
+     * @return user
+     */
     public User getUser(String email);
 }
