6f3b99fd0eb3f93499d146cbd69ae43dddeb8c30
Tracy Chen <stchen1012@gmail.com>
2019-12-03 11:53:03 -0500

    api gateway tests

--- api-gateway/src/test/java/com/example/apigateway/config/JwtRequestFilterTest.java
+++ api-gateway/src/test/java/com/example/apigateway/config/JwtRequestFilterTest.java
@@ -1,22 +1,77 @@
-//package com.example.apigateway.config;
-//
-//import com.example.apigateway.Service.UserService;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class JwtRequestFilterTest {
-//
-//    @Mock
-//    UserService userService;
-//
-//    @Mock
-//    JwtUtil jwtUtil;
-//
-//    @InjectMocks
-//    JwtRequestFilter jwtRequestFilter;
-//
-//
-//}
+package com.example.apigateway.config;
+
+import com.example.apigateway.Service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class JwtRequestFilterTest {
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    JwtUtil jwtUtil;
+
+    @InjectMocks
+    JwtRequestFilter jwtRequestFilter;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpServletResponse response;
+
+    @Mock
+    FilterChain chain;
+
+    @Mock
+    UserDetails userDetails;
+
+    @Test
+    public void doFilterInternal_SUCCESS() throws ServletException, IOException {
+        when(request.getHeader(any())).thenReturn("Bearer 1234");
+        when(jwtUtil.getUsernameFromToken(any())).thenReturn("test");
+        when(userService.loadUserByUsername(any())).thenReturn(userDetails);
+        when(jwtUtil.validateToken(any(), any())).thenReturn(true);
+
+        jwtRequestFilter.doFilterInternal(request, response, chain);
+    }
+
+    @Test
+    public void doFilterInternal_FAIL() throws ServletException, IOException {
+        when(request.getHeader(any())).thenReturn("1234");
+        when(jwtUtil.getUsernameFromToken(any())).thenReturn("test");
+        when(userService.loadUserByUsername(any())).thenReturn(userDetails);
+        when(jwtUtil.validateToken(any(), any())).thenReturn(true);
+
+        jwtRequestFilter.doFilterInternal(request, response, chain);
+    }
+
+    @Test
+    public void doFilterInternal_IllegalArgumentException_FAIL() throws ServletException, IOException {
+        when(request.getHeader(any())).thenReturn("Bearer 1234");
+        when(jwtUtil.getUsernameFromToken(any())).thenThrow(IllegalArgumentException.class);
+        jwtRequestFilter.doFilterInternal(request, response, chain);
+    }
+
+    @Test
+    public void doFilterInternal_ExpiredJWTException_FAIL() throws ServletException, IOException {
+        when(request.getHeader(any())).thenReturn("Bearer 1234");
+        when(jwtUtil.getUsernameFromToken(any())).thenThrow(ExpiredJwtException.class);
+        jwtRequestFilter.doFilterInternal(request, response, chain);
+    }
+}
